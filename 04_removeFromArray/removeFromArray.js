082643ddd887fc11995fc884891dc2c387cec210
awrelyah <aureliamihkk@gmail.com>
2022-05-28 20:00:33 +0300

    4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, argument, ...theArgs) {
+    if (theArgs.length == 0){
+        const newArray = array.filter(item => item !== argument);
+        return newArray;
+
+    } else if (theArgs.length >= array.length - 1){ // removes all elements
+        array = [];
+        return array;
+
+    } else if(theArgs.length > 0){
+        const newArray = array.filter(item => item !== argument);
+        let newArray2 = [];
+        
+        for (i=0; i< theArgs.length; i++){
+            newArray2 = newArray.filter(item => item !== theArgs[i]);
+        }
+        return newArray2;
+
+    } 
 
 };
 
