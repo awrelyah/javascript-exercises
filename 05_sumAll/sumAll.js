83cb9c51706d820e8130934ebbdffa1d906361c5
awrelyah <aureliamihkk@gmail.com>
2022-05-28 20:17:27 +0300

    5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof  num2 !== "number") {
+        return "ERROR";
+    } else if (num2 >= num1){
+        for (i = num1; i <= num2; i++){
+            finalSum += i;
+        }
+    return finalSum;
+    } else if (num1 > num2){
+        for (i = num2; i <= num1; i++){
+            finalSum += i;
+        }
+        return finalSum;
+    } 
 
 };
 
