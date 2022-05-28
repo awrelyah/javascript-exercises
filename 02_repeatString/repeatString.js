99a10d81f62db916d56635fa0e518755262eb711
awrelyah <aureliamihkk@gmail.com>
2022-05-28 18:43:29 +0300

    2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let newText = "";
+    if (num < 0){
+        return "ERROR"
+    }
+    for (i = 0; i < num; i++){
+        newText += text;
+    }
+    return newText;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
